013d8e0a23d058603fdba1ad17227d6599507634
syed mohammed <syedmohammedn22@gmail.com>
2020-07-18 22:25:35 +0530

    line chart

--- src/components/common/lineChart.jsx
+++ src/components/common/lineChart.jsx
@@ -7,11 +7,11 @@ class LineChart extends Component {
     datasets: [
       {
         label: "Rainfall",
-        fill: false,
+        fill: true,
         lineTension: 0.5,
         backgroundColor: "rgba(75,192,192,1)",
         borderColor: "rgba(0,0,0,1)",
-        borderWidth: 2,
+        borderWidth: 5,
         data: [65, 59, 80, 81, 56],
       },
     ],
