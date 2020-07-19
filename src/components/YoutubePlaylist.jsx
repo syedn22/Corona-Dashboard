f4ddda96f20b030f12141ea12cacdd84fc18beb5
syed mohammed <syedmohammedn22@gmail.com>
2020-07-19 21:44:02 +0530

    Youtube video rendered

--- src/components/YoutubePlaylist.jsx
+++ src/components/YoutubePlaylist.jsx
@@ -11,10 +11,45 @@ class YoutubePlaylist extends Component {
     console.log(result.data.items);
     this.setState({ playList: result.data.items });
   }
+  handleKey(item) {
+    // console.log( item.snippet.description.);
+    return item.id;
+  }
+
+  handleClick(item) {
+    console.log(item);
+  }
+
   render() {
+    const { playList } = this.state;
     return (
-      <div className="card-header">
-        <div className="card-body"></div>
+      <div className="container">
+        {playList.map((item) =>
+          item.snippet.description ? (
+            <div className="card bg-light m-3" key={this.handleKey(item)}>
+              <div className="row">
+                <div className="col">
+                  <img
+                    className="card-img-top img-width float-left m-4"
+                    src={item.snippet.thumbnails.high.url}
+                    alt="Card image cap"
+                  ></img>
+                </div>
+                <div className="col">
+                  <div className="card-body">
+                    <h5 className="card-title">{item.snippet.title}</h5>
+                    <p className="card-text ">
+                      {item.snippet.description.substring(0, 500)}
+                    </p>
+                    <button className="btn btn-primary" onClick={()=>this.handleClick(item)}>Watch Video</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )
+        )}
       </div>
     );
   }
